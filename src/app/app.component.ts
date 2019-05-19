b46d082e7d1e8660c935c4731e0d933bb34475ee
Hevski <heathercmalloch@gmail.com>
2019-05-19 10:19:24 +0100

    initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'RPSLS';
+  title = 'Rock Paper Scissors Lizzard Spock!';
 }

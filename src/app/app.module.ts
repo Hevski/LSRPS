de6f10d39172558f8562c8c705d99aa6e3fa62e7
Hevski <heathercmalloch@gmail.com>
2019-05-19 12:46:31 +0100

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
+import { PlayerComponent } from './components/player/player.component';
+import { GameContainerComponent } from './containers/game-container/game-container.component';
+import { TitleComponent } from './components/title/title.component';
+import { DisplayComponent } from './components/display/display.component';
+import { ButtonComponent } from './components/button/button.component';
+import { ChoiceComponent } from './components/choice/choice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerComponent,
+    GameContainerComponent,
+    TitleComponent,
+    DisplayComponent,
+    ButtonComponent,
+    ChoiceComponent,
   ],
   imports: [
     BrowserModule
